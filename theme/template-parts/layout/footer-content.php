93324723f6976bb082234e0f82921a30c3e7eb86
Belinda Caylor <belinda@thekestrel.co>
2024-03-08 08:47:03 -0600

    Base Tailwind Theme Setup

---
+++ theme/template-parts/layout/footer-content.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template part for displaying the footer content
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Base_Tailwind
+ */
+
+?>
+
+<footer id="colophon">
+
+</footer><!-- #colophon -->
---
+++ theme/template-parts/layout/header-content.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template part for displaying the header content
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Base_Tailwind
+ */
+
+?>
+
+<header id="masthead">
+
+    <nav id="site-navigation" aria-label="<?php esc_attr_e( 'Main Navigation', 'client_name_here' ); ?>">
+            
+    </nav><!-- #site-navigation -->
+
+</header><!-- #masthead -->
